4936f6f416150a52952c2e5ebfd9d330cc427333
Subhangee Sahoo <shubh190601@gmail.com>
2024-12-27 15:31:24 +0530

    Updated

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,5 +1,4 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
 import './Node.css';
 
 export default class Node extends Component {
@@ -14,6 +13,8 @@ export default class Node extends Component {
       onMouseUp,
       row,
     } = this.props;
+
+    // Determine the CSS class for the node
     const extraClassName = isFinish
       ? 'node-finish'
       : isStart
@@ -24,11 +25,12 @@ export default class Node extends Component {
 
     return (
       <div
-        id={`node-${row}-${col}`}
-        className={`node ${extraClassName}`}
-        onMouseDown={() => onMouseDown(row, col)}
-        onMouseEnter={() => onMouseEnter(row, col)}
-        onMouseUp={() => onMouseUp()}></div>
+        id={`node-${row}-${col}`} // Unique identifier for each node
+        className={`node ${extraClassName}`} // Apply appropriate CSS class
+        onMouseDown={() => onMouseDown(row, col)} // Mouse down event
+        onMouseEnter={() => onMouseEnter(row, col)} // Mouse enter event
+        onMouseUp={() => onMouseUp()} // Mouse up event
+      ></div>
     );
   }
 }
